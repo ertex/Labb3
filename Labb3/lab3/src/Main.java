2563b6571fa244bb602b3e7134ea6aec60ab6916
EmilieKar <Emilie.karon.klefbom@gmail.com>
2017-10-11 11:33:07 +0200

    + tempo

--- Labb3/lab3/src/Main.java
+++ Labb3/lab3/src/Main.java
@@ -5,17 +5,25 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException{
+    public static void main(String[] args) throws FileNotFoundException {
+        double tempo = 148.0;//eitghnotes /min
         File file = new File("src\\elise.txt");
         Scanner sc = new Scanner(file);
         SoundDevice device = new SoundDevice();
-        Song song = new Song(10);
-        while(sc.hasNext()){
-            song.add(MusicUtils.harmonic(sc.nextInt(),sc.nextDouble()));
+        Song song = new Song(25);
+        while (sc.hasNext()) {
+            song.add(MusicUtils.harmonic(sc.nextInt(),parseTempo(sc.nextDouble(),tempo)));
+
         }
         song.play(device);
-        song.save(device.getFormat(),new File("twotones.wav"));
+        song.save(device.getFormat(), new File("twotones.wav"));
+
+
     }//main
 
+    public static double parseTempo(double duration, double tempo) {
+        return (tempo/60) * duration;
+
+    }
 
 }//Main
