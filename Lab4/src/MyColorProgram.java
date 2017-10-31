0537b8859f455dcc62a13703533a4829142aa292
ertex <davidlgjohansson@gmail.com>
2017-10-31 15:46:56 +0100

    everything except 2.3 is done

--- Lab4/src/MyColorProgram.java
+++ Lab4/src/MyColorProgram.java
@@ -1,17 +1,130 @@
-public class MyColorProgram { 
-   public static void main(String[] args) throws Exception { 
-      int[][][] original = ColorImage.read("mushroom.jpeg"); 
-      int[][][] manipulated = upDown(original); 
-      ColorImage.write("upDownMushroom.jpeg", manipulated); 
-      ColorImageWindow iw = new ColorImageWindow(original, manipulated); 
-   }//main 
+public class MyColorProgram {
+    public static void main(String[] args) throws Exception {
+        int[][][] original = ColorImage.read("mushroom.jpeg");
+        int[][][] manipulated = sharpenTwo(original);
+        ColorImage.write("upDownMushroom.jpeg", manipulated);
+        ColorImageWindow iw = new ColorImageWindow(original, manipulated);
+    }//main
 
-   public static int[][][] upDown(int[][][] samples) { 
-      int[][][] newSamples = new int[samples.length][samples[0].length][3]; 
-      for (int row = 0; row < samples.length; row = row + 1)
-         for (int col = 0; col < samples[row].length; col = col + 1) 
-            for (int c = 0; c < samples[row][col].length; c = c + 1) 
-               newSamples[row][col][c] = samples[samples.length-row-1][col][c]; 
-      return newSamples; 
-   }//upDown 
+    public static int[][][] upDown(int[][][] samples) {
+        int[][][] newSamples = new int[samples.length][samples[0].length][3];
+        for (int row = 0; row < samples.length; row = row + 1)
+            for (int col = 0; col < samples[row].length; col = col + 1)
+                for (int c = 0; c < samples[row][col].length; c = c + 1)
+                    newSamples[row][col][c] = samples[samples.length - row - 1][col][c];
+        return newSamples;
+    }//upDown
+
+    public static int[][][] invert(int[][][] samples) {
+        int[][][] newSamples = new int[samples.length][samples[0].length][samples[0][0].length];
+        for (int row = 0; row < samples.length; row = row + 1)
+            for (int col = 0; col < samples[row].length; col = col + 1)
+                for (int color = 0; color < samples[row][col].length; color = color + 1)
+                    newSamples[row][col][color] = 255 - samples[row][col][color];
+        return newSamples;
+
+    }//Invert
+
+    public static int[][][] toGrey(int[][][] samples) {
+        int[][][] newSamples = new int[samples.length][samples[0].length][samples[0][0].length];
+        for (int row = 0; row < samples.length; row = row + 1) {
+            for (int col = 0; col < samples[row].length; col = col + 1) {
+
+                int luminosoty = (int) (
+                        samples[row][col][0] * 0.299 +
+                                samples[row][col][1] * 0.587 +
+                                samples[row][col][2] * 0.114);
+                for (int color = 0; color < 3; color++) {
+                    newSamples[row][col][color] = luminosoty;
+
+                }
+            }
+        }
+        return newSamples;
+    }//toGrey
+
+    public static int[][][] blackWhite(int[][][] samples) {
+        int[][][] newSamples = new int[samples.length][samples[0].length][samples[0][0].length];
+        for (int row = 0; row < samples.length; row = row + 1) {
+            for (int col = 0; col < samples[row].length; col = col + 1) {
+
+                int luminosoty = (int) (
+                        samples[row][col][0] * 0.299 +
+                                samples[row][col][1] * 0.587 +
+                                samples[row][col][2] * 0.114);
+
+                for (int color = 0; color < 3; color++) {
+                    if (luminosoty > 128) {
+                        newSamples[row][col][color] = 255;
+                    } else {
+                        newSamples[row][col][color] = 0;
+
+                    }
+                }
+            }
+        }
+        return newSamples;
+    }//BlackWhite
+
+    public static int[][][] sharpenOne(int[][][] samples) {
+        int[][][] newSamples = new int[samples.length][samples[0].length][samples[0][0].length];
+        for (int row = 0; row < samples.length; row++) {
+            for (int col = 0; col < samples[row].length; col++) {
+
+                for (int color = 0; color < samples[row][col].length; color++) {
+
+                    newSamples[row][col][color] = 10 * samples[row][col][color];
+                    //adding 10samples instead of 9 and then sumbtracting them again, o decreeese code volume
+                    if (row > 1 & row < (samples.length - 1) & (col > 1 & col < ((samples[row].length - 1)))) {
+                        //note that the for loops start at -1
+                        for (int i = -1; i < 2; i++) {
+                            for (int j = -1; j < 2; j++) {
+                                newSamples[row][col][color] -= samples[row + i][col + j][color];
+                                //subtracts the surrounding 8 pixels AND the pixel in [i][j]
+                            }
+                        }
+                        if (newSamples[row][col][color] > 255) {
+                            newSamples[row][col][color] = 255;
+                        } else if (newSamples[row][col][color] < 0) {
+                            newSamples[row][col][color] = 0;
+
+                        }
+                    }
+                }
+            }
+        }
+        return newSamples;
+
+    }//sharpenOne
+
+    public static int[][][] sharpenTwo(int[][][] samples) {
+        int[][][] newSamples = new int[samples.length][samples[0].length][samples[0][0].length];
+        for (int row = 0; row < samples.length; row++) {
+            for (int col = 0; col < samples[row].length; col++) {
+
+                for (int color = 0; color < samples[row][col].length; color++) {
+
+                    newSamples[row][col][color] = 10 * samples[row][col][color];
+                    //adding 10samples instead of 9 and then sumbtracting them again, o decreeese code volume
+                    if (row > 1 & row < (samples.length - 1) & (col > 1 & col < ((samples[row].length - 1)))) {
+                        newSamples[row][col][color] = 5 * samples[row][col][color] - (
+                                        samples[row - 1][col][color] +
+                                        samples[row + 1][col][color] +
+                                        samples[row][col - 1][color] +
+                                        samples[row][col + 1][color] );
+
+                        if (newSamples[row][col][color] > 255) {
+                            newSamples[row][col][color] = 255;
+                        } else if (newSamples[row][col][color] < 0) {
+                            newSamples[row][col][color] = 0;
+
+                        }
+                    }
+                }
+            }
+        }
+        return newSamples;
+
+    }//sharpenTwo
+
 }//MyColorProgram
--- Lab4/src/MyGrayProgram.java
+++ Lab4/src/MyGrayProgram.java
@@ -1,16 +1,71 @@
-public class MyGrayProgram { 
-   public static void main(String[] args) throws Exception{ 
-      int[][] original = GrayImage.read("mushroom.jpeg"); 
-      int[][] manipulated = upDown(original); 
-      GrayImage.write("upDownMushroom.jpeg", manipulated); 
-      GrayImageWindow iw = new GrayImageWindow(original, manipulated); 
-   }//main 
+public class MyGrayProgram {
+    public static void main(String[] args) throws Exception {
+        int[][] original = GrayImage.read("mushroom.jpeg");
+        int[][] manipulated = upDown(original);
+        GrayImage.write("upDownMushroom.jpeg", manipulated);
+        GrayImageWindow iw = new GrayImageWindow(original, manipulated);
+    }//main
 
-   public static int[][] upDown(int[][] samples) { 
-      int[][] newSamples = new int[samples.length][samples[0].length]; 
-      for (int row = 0; row < samples.length; row = row + 1) 
-         for (int col = 0; col < samples[row].length; col = col + 1) 
-            newSamples[row][col] = samples[samples.length - row - 1 ][col]; 
-      return newSamples; 
-    }//upDown 
+
+    public static int[][] upDown(int[][] samples) {
+        int[][] newSamples = new int[samples.length][samples[0].length];
+        for (int row = 0; row < samples.length; row = row + 1)
+            for (int col = 0; col < samples[row].length; col = col + 1)
+                newSamples[row][col] = samples[samples.length - row - 1][col];
+        return newSamples;
+    }//upDown
+
+    public static int[][] leftRight(int[][] samples) {
+        int[][] newSamples = new int[samples.length][samples[0].length];
+        for (int row = 0; row < samples.length; row = row + 1)
+            for (int col = 0; col < samples[row].length; col = col + 1)
+                newSamples[row][col] = samples[row][samples[row].length - col - 1];
+        return newSamples;
+    }//LeftRght
+
+    public static int[][] invert(int[][] samples) {
+        int[][] newSamples = new int[samples.length][samples[0].length];
+        for (int row = 0; row < samples.length; row = row + 1)
+            for (int col = 0; col < samples[row].length; col = col + 1)
+                newSamples[row][col] = 255 - samples[row][col];
+        return newSamples;
+
+    }//Invert
+
+    public static int[][] toBlackWhite(int[][] samples) {
+        int[][] newSamples = new int[samples.length][samples[0].length];
+        for (int row = 0; row < samples.length; row = row + 1)
+            for (int col = 0; col < samples[row].length; col = col + 1)
+                if (samples[row][col] > 128) {
+                    newSamples[row][col] = 255;
+                } else {
+                    newSamples[row][col] = 0;
+
+                }
+        return newSamples;
+
+    }//toBlackWhite
+
+    public static int[][] contour(int[][] samples) {
+        int[][] blackWhite = toBlackWhite(samples);
+        int[][] newSamples = new int[samples.length][samples[0].length];
+        for (int row = 0; row < blackWhite.length; row = row + 1) {
+            for (int col = 0; col < blackWhite[row].length; col = col + 1) {
+                //makes sure it's not out of bounds
+                if (row > 1 & row < (blackWhite.length - 1) & (col > 1 & col < ((blackWhite[row].length - 1)))) {
+                    //checks for countour
+                    if (blackWhite[row][col] == 255 &
+                            (blackWhite[row + 1][col] == 0
+                                    | blackWhite[row - 1][col] == 0
+                                    | blackWhite[row][col + 1] == 0
+                                    | blackWhite[row][col - 1] == 0)) {
+                        newSamples[row][col] = 0;
+                    } else {
+                        newSamples[row][col] = 255;
+                    }
+                }
+            }
+        }
+        return newSamples;
+    }//countour
 }//MyGrayProgram
