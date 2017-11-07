b17af8600eb9bfbdbb288de93db0d705d0f8d439
EmilieKar <Emilie.karon.klefbom@gmail.com>
2017-11-07 12:08:23 +0100

    shel

---
+++ Labb5/src/Lab5_skel.java
@@ -0,0 +1,65 @@
+public class Lab5_skel {
+
+  public static void main(String[] args) {
+    // -- Del 1 --
+
+    doCommandLine();
+    //System.out.println(toRobber("Hej på dig"));
+    //System.out.println(toPigLatin("Hej på dig"));
+
+
+    // -- Del 2 --
+
+    //rollADice();
+    //letPlayerRoll();
+    //letTwoPlayersRollSameDice();
+    //letPlayerUseDiceCup();
+    //findPlayerWithMax();
+  }
+
+  // ---------- Del 1 ---------------
+
+  // 1 and 4
+  public static void doCommandLine() {
+  }
+
+  // 2
+  public static String toRobber(String text) {
+    return null; // Just for now
+  }
+
+  // 3
+  public static boolean isVowel(char ch) {
+    return false;  // Just for now
+  }
+
+  public static String toPigLatin(String text) {
+    return null;// Just for now
+  }
+
+
+  // ---------- Del 2 ---------------
+
+  // 5
+  public static void rollADice() {
+  }
+
+  // 6
+  public static void letPlayerRoll() {
+  }
+
+  // 7
+  public static void letTwoPlayersRollSameDice() {
+  }
+
+  // 8
+  public static void letPlayerUseDiceCup() {
+  }
+
+  // 9
+  // Same as above but toString overridden.
+  
+  // 10
+  public static void findPlayerWithMax() {
+  }
+}
