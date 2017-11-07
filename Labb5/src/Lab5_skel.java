94801ac31a199c976af2cf5e69a5406767d0b99e
EmilieKar <Emilie.karon.klefbom@gmail.com>
2017-11-07 14:12:16 +0100

    Rätt ihop lagda

--- Labb5/src/Lab5_skel.java
+++ Labb5/src/Lab5_skel.java
@@ -9,8 +9,7 @@ public class Lab5_skel {
         //System.out.println(toRobber("Hej på dig"));
         //System.out.println(toPigLatin("Hej på dig"));
 
-
-<<<<<<< HEAD
+        
     // -- Del 2 --
     //rollADice();
       Dice dice = new Dice(6);
@@ -44,96 +43,6 @@ public class Lab5_skel {
     //findPlayerWithMax();
   }
 
-  // ---------- Del 1 ---------------
-
-  // 1 and 4
-  public static void doCommandLine() {
-      String lastReceived;
-      boolean looping = true;
-      do {
-          lastReceived = scanner.next();
-          switch (lastReceived) {
-              case "p":
-                  System.out.println("Input text");
-                  lastReceived = scanner.next();
-                  System.out.println(lastReceived);
-                  break;
-              case "r":
-                  System.out.println("Input text");
-                  lastReceived = scanner.next();
-                  System.out.println(lastReceived);
-                  break;
-
-              case "q":
-                  looping = false;
-                  break;
-          }
-
-      } while (looping);
-  }
-
-  // 2
-  public static String toRobber(String text) {
-    return null; // Just for now
-  }
-
-  // 3
-  public static boolean isVowel(char ch) {
-    return false;  // Just for now
-  }
-
-  public static String toPigLatin(String text) {
-    return null;// Just for now
-  }
-
-
-  // ---------- Del 2 ---------------
-
-  // 5
-  public static int rollADice(Dice dice) {
-    int num = dice.roll();
-    return num;
-  }
-
-  // 6
-  public static void letPlayerRoll(Player1 player, int numRolls) {
-    player.rollDice(numRolls);
-  }
-
-  // 7
-  public static void letTwoPlayersRollSameDice(Dice dice, String name1, String name2,  int numRolls) {
-    Player1 player1 = new Player1(name1, dice);
-    Player1 player2 = new Player1(name2, dice);
-    letPlayerRoll(player1 , numRolls);
-    letPlayerRoll(player2 , numRolls);
-  }
-
-  // 8
-  public static void letPlayerUseDiceCup(Player2 player) {
-      player.player2roll();
-  }
-
-  // 9
-  // Same as above but toString overridden.
-  
-  // 10
-  public static void findPlayerWithMax() {
-  }
-=======
-        // -- Del 2 --
-
-        //rollADice();
-
-        //letPlayerRoll();
-       Dice d = new Dice(6);
-        for (int i = 0; i < 100; i++) {
-            System.out.print(d.roll());
-        }
-        //letTwoPlayersRollSameDice();
-        //letPlayerUseDiceCup();
-        //findPlayerWithMax();
-    }
-
     // ---------- Del 1 ---------------
 
     // 1 and 4
@@ -153,7 +62,7 @@ public class Lab5_skel {
                 case "r":
                     System.out.println("Input text");
                     lastReceived = scanner.next();
-                   // System.out.println(lastReceived);
+                    // System.out.println(lastReceived);
                     toRobber(lastReceived);
                     break;
 
@@ -188,47 +97,39 @@ public class Lab5_skel {
         return "AEIOUÅÄÖaeiouåäö".indexOf(ch) != -1;
     }
 
-
-
-
-
-
-
     public static String toPigLatin(String text) {
         return null;// Just for now
     }
 
+  // ---------- Del 2 ---------------
 
-    // ---------- Del 2 ---------------
+  // 5
+  public static int rollADice(Dice dice) {
+    int num = dice.roll();
+    return num;
+  }
 
-    // 5
-    public static int rollADice(Dice dice) {
-        int num = dice.roll();
-        return num;
-    }
+  // 6
+  public static void letPlayerRoll(Player1 player, int numRolls) {
+    player.rollDice(numRolls);
+  }
 
-    // 6
-    public static void letPlayerRoll(Player1 player, int numRolls) {
-        player.rollDice(numRolls);
-    }
+  // 7
+  public static void letTwoPlayersRollSameDice(Dice dice, String name1, String name2,  int numRolls) {
+    Player1 player1 = new Player1(name1, dice);
+    Player1 player2 = new Player1(name2, dice);
+    letPlayerRoll(player1 , numRolls);
+    letPlayerRoll(player2 , numRolls);
+  }
 
-    // 7
-    public static void letTwoPlayersRollSameDice(Dice dice, String name1, String name2, int numRolls) {
-        Player1 player1 = new Player1(name1, dice);
-        Player1 player2 = new Player1(name2, dice);
-        letPlayerRoll(player1, numRolls);
-        letPlayerRoll(player2, numRolls);
-    }
+  // 8
+  public static void letPlayerUseDiceCup(Player2 player) {
+      player.player2roll();
+  }
 
-    // 8
-    public static void letPlayerUseDiceCup() {
-    }
-
-    // 9
-    // Same as above but toString overridden.
-
-    // 10
-    public static void findPlayerWithMax() {
-    }
->>>>>>> 23123f409db828d31dae558d2ec64389606fe1a1
-}
+  // 9
+  // Same as above but toString overridden.
+  
+  // 10
+  public static void findPlayerWithMax() {
+  }
