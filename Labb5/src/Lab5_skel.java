23123f409db828d31dae558d2ec64389606fe1a1
David <davidlgjohansson@gmail.com>
2017-11-07 14:00:41 +0100

    does robber now (upg 2)

--- Labb5/src/Lab5_skel.java
+++ Labb5/src/Lab5_skel.java
@@ -1,100 +1,123 @@
+import java.util.Scanner;
 
 public class Lab5_skel {
 
-  public static void main(String[] args) {
-    // -- Del 1 --
+    public static void main(String[] args) {
+        // -- Del 1 --
 
-    doCommandLine();
-    //System.out.println(toRobber("Hej på dig"));
-    //System.out.println(toPigLatin("Hej på dig"));
+        doCommandLine();
+        //System.out.println(toRobber("Hej på dig"));
+        //System.out.println(toPigLatin("Hej på dig"));
 
 
-    // -- Del 2 --
+        // -- Del 2 --
 
-    //rollADice();
+        //rollADice();
 
-    //letPlayerRoll();
-    Dice d = new Dice(6);
-    for(int i = 0; i <100; i++){
-      System.out.print(d.roll());
+        //letPlayerRoll();
+       Dice d = new Dice(6);
+        for (int i = 0; i < 100; i++) {
+            System.out.print(d.roll());
+        }
+        //letTwoPlayersRollSameDice();
+        //letPlayerUseDiceCup();
+        //findPlayerWithMax();
     }
-    //letTwoPlayersRollSameDice();
-    //letPlayerUseDiceCup();
-    //findPlayerWithMax();
-  }
 
-  // ---------- Del 1 ---------------
+    // ---------- Del 1 ---------------
 
-  // 1 and 4
-  public static void doCommandLine() {
-      String lastReceived;
-      boolean looping = true;
-      do {
-          lastReceived = scanner.next();
-          switch (lastReceived) {
-              case "p":
-                  System.out.println("Input text");
-                  lastReceived = scanner.next();
-                  System.out.println(lastReceived);
-                  break;
-              case "r":
-                  System.out.println("Input text");
-                  lastReceived = scanner.next();
-                  System.out.println(lastReceived);
-                  break;
+    // 1 and 4
+    static Scanner scanner = new Scanner(System.in);
 
-              case "q":
-                  looping = false;
-                  break;
-          }
+    public static void doCommandLine() {
+        String lastReceived;
+        boolean looping = true;
+        do {
+            lastReceived = scanner.next();
+            switch (lastReceived) {
+                case "p":
+                    System.out.println("Input text");
+                    lastReceived = scanner.next();
+                    System.out.println(lastReceived);
+                    break;
+                case "r":
+                    System.out.println("Input text");
+                    lastReceived = scanner.next();
+                   // System.out.println(lastReceived);
+                    toRobber(lastReceived);
+                    break;
 
-      } while (looping);
-  }
+                case "q":
+                    looping = false;
+                    break;
+            }
 
-  // 2
-  public static String toRobber(String text) {
-    return null; // Just for now
-  }
+        } while (looping);
+    }
 
-  // 3
-  public static boolean isVowel(char ch) {
-    return false;  // Just for now
-  }
+    // 2
+    public static String toRobber(String text) {
+        for (int i : text.getBytes()) {
+            char ch = text.charAt(i);
+            if (isVowel(ch)) { //if the char at index i is a wovel, print the char
+                System.out.print(ch);
+            } else {        //else if if if its !vowel (konsonant?) surround a "o" with the char
+                System.out.print(ch + "o" + ch);
+            }
 
-  public static String toPigLatin(String text) {
-    return null;// Just for now
-  }
+        }
 
 
-  // ---------- Del 2 ---------------
+        return null; // Just for now
+    }
 
-  // 5
-  public static int rollADice(Dice dice) {
-    int num = dice.roll();
-    return num;
-  }
+    // 3
+    public static boolean isVowel(char ch) {
 
-  // 6
-  public static void letPlayerRoll(Player1 player, int numRolls) {
-    player.rollDice(numRolls);
-  }
 
-  // 7
-  public static void letTwoPlayersRollSameDice(Dice dice, String name1, String name2,  int numRolls) {
-    Player1 player1 = new Player1(name1, dice);
-    Player1 player2 = new Player1(name2, dice);
-    letPlayerRoll(player1 , numRolls);
-    letPlayerRoll(player2 , numRolls);
-  }
+        return "AEIOUÅÄÖaeiouåäö".indexOf(ch) != -1;
+    }
 
-  // 8
-  public static void letPlayerUseDiceCup() {
-  }
 
-  // 9
-  // Same as above but toString overridden.
-  
-  // 10
-  public static void findPlayerWithMax() {
-  }
+
+
+
+
+
+    public static String toPigLatin(String text) {
+        return null;// Just for now
+    }
+
+
+    // ---------- Del 2 ---------------
+
+    // 5
+    public static int rollADice(Dice dice) {
+        int num = dice.roll();
+        return num;
+    }
+
+    // 6
+    public static void letPlayerRoll(Player1 player, int numRolls) {
+        player.rollDice(numRolls);
+    }
+
+    // 7
+    public static void letTwoPlayersRollSameDice(Dice dice, String name1, String name2, int numRolls) {
+        Player1 player1 = new Player1(name1, dice);
+        Player1 player2 = new Player1(name2, dice);
+        letPlayerRoll(player1, numRolls);
+        letPlayerRoll(player2, numRolls);
+    }
+
+    // 8
+    public static void letPlayerUseDiceCup() {
+    }
+
+    // 9
+    // Same as above but toString overridden.
+
+    // 10
+    public static void findPlayerWithMax() {
+    }
 }
