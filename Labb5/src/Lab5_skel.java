207c7f5f57b0942bff10c01dc21162ff14893b12
David <davidlgjohansson@gmail.com>
2017-11-07 13:23:27 +0100

    Upg1 klar

--- Labb5/src/Lab5_skel.java
+++ Labb5/src/Lab5_skel.java
@@ -1,65 +1,90 @@
+import java.util.Scanner;
+
 public class Lab5_skel {
+    static Scanner scanner = new Scanner(System.in);
 
-  public static void main(String[] args) {
-    // -- Del 1 --
+    public static void main(String[] args) {
+        // -- Del 1 --
 
-    doCommandLine();
-    //System.out.println(toRobber("Hej på dig"));
-    //System.out.println(toPigLatin("Hej på dig"));
-
-
-    // -- Del 2 --
-
-    //rollADice();
-    //letPlayerRoll();
-    //letTwoPlayersRollSameDice();
-    //letPlayerUseDiceCup();
-    //findPlayerWithMax();
-  }
-
-  // ---------- Del 1 ---------------
-
-  // 1 and 4
-  public static void doCommandLine() {
-  }
-
-  // 2
-  public static String toRobber(String text) {
-    return null; // Just for now
-  }
-
-  // 3
-  public static boolean isVowel(char ch) {
-    return false;  // Just for now
-  }
-
-  public static String toPigLatin(String text) {
-    return null;// Just for now
-  }
+        doCommandLine();
+        //System.out.println(toRobber("Hej på dig"));
+        //System.out.println(toPigLatin("Hej på dig"));
 
 
-  // ---------- Del 2 ---------------
+        // -- Del 2 --
 
-  // 5
-  public static void rollADice() {
-  }
+        //rollADice();
+        //letPlayerRoll();
+        //letTwoPlayersRollSameDice();
+        //letPlayerUseDiceCup();
+        //findPlayerWithMax();
+    }
 
-  // 6
-  public static void letPlayerRoll() {
-  }
+    // ---------- Del 1 ---------------
 
-  // 7
-  public static void letTwoPlayersRollSameDice() {
-  }
+    // 1 and 4
+    public static void doCommandLine() {
+        String lastReceived;
+        boolean looping = true;
+        do {
+            lastReceived = scanner.next();
+            switch (lastReceived) {
+                case "p":
+                    System.out.println("Input text");
+                    lastReceived = scanner.next();
+                    System.out.println(lastReceived);
+                    break;
+                case "r":
+                    System.out.println("Input text");
+                    lastReceived = scanner.next();
+                    System.out.println(lastReceived);
+                    break;
 
-  // 8
-  public static void letPlayerUseDiceCup() {
-  }
+                case "q":
+                    looping = false;
+                    break;
+            }
 
-  // 9
-  // Same as above but toString overridden.
-  
-  // 10
-  public static void findPlayerWithMax() {
-  }
+        } while (looping);
+    }
+
+    // 2
+    public static String toRobber(String text) {
+        return null; // Just for now
+    }
+
+    // 3
+    public static boolean isVowel(char ch) {
+        return false;  // Just for now
+    }
+
+    public static String toPigLatin(String text) {
+        return null;// Just for now
+    }
+
+
+    // ---------- Del 2 ---------------
+
+    // 5
+    public static void rollADice() {
+    }
+
+    // 6
+    public static void letPlayerRoll() {
+    }
+
+    // 7
+    public static void letTwoPlayersRollSameDice() {
+    }
+
+    // 8
+    public static void letPlayerUseDiceCup() {
+    }
+
+    // 9
+    // Same as above but toString overridden.
+
+    // 10
+    public static void findPlayerWithMax() {
+    }
 }
